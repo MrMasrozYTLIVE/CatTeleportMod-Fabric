9d91ebe76debfaf10d5b41a710625e8362f6f5e2
MrMasrozYTLIVE <61359286+MrMasrozYTLIVE@users.noreply.github.com>
2024-11-28 17:47:13 +0300

    New update

---
+++ changelog.md
@@ -0,0 +1,8 @@
+# Support for other mods with /ctp command
+(I am talking about you, ClientCommands)
+Now mod has 3 commands (/ctp, /cattp and /catteleport) which you can use 
+
+# Fixed some problems with ModPublisher
+
+# Disabled publishing for CurseForge
+I have no plans on supporting it anymore, doesn't work it.
--- src/main/java/net/just_s/ctpmod/util/CommandRegistry.java
+++ src/main/java/net/just_s/ctpmod/util/CommandRegistry.java
@@ -4,12 +4,13 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.tree.ArgumentCommandNode;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.just_s.ctpmod.commands.*;
 import net.minecraft.command.CommandRegistryAccess;
+
+import java.util.List;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
@@ -17,41 +18,50 @@ import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.lit
 public class CommandRegistry {
     public static void register() {
         ClientCommandRegistrationCallback.EVENT.register((CommandDispatcher<FabricClientCommandSource> dispatcher, CommandRegistryAccess registryAccess) -> {
-            LiteralCommandNode<FabricClientCommandSource> mainCommand = literal("ctp").build();
+            var subs = generateSubcommands();
 
-            // List SubCommand
-            LiteralCommandNode<FabricClientCommandSource> listNode = literal("list").executes(ListCommand::run).build();
-
-            // RC SubCommand
-            LiteralCommandNode<FabricClientCommandSource> rcNode = literal("rc").build();
-            rcNode.addChild(argument("seconds", IntegerArgumentType.integer(1))
-                    .executes(RcCommand::run).build());
-
-            // TP SubCommand
-            LiteralCommandNode<FabricClientCommandSource> tpNode = literal("tp").build();
-            tpNode.addChild(argument("name", StringArgumentType.word())
-                    .suggests(TpCommand::suggest).executes(TpCommand::run).build());
-
-            // Delete SubCommand
-            LiteralCommandNode<FabricClientCommandSource> deleteNode = literal("delete").build();
-            deleteNode.addChild(argument("name", StringArgumentType.word())
-                    .suggests(DeleteCommand::suggest).executes(DeleteCommand::run).build());
-
-            // Add SubCommand
-            LiteralCommandNode<FabricClientCommandSource> addNode = literal("add").build();
-            ArgumentCommandNode<FabricClientCommandSource, String> nameArgument = argument("name", StringArgumentType.word()).build();
-            ArgumentCommandNode<FabricClientCommandSource, Float> startArgument = argument("startPeriod", FloatArgumentType.floatArg(0)).build();
-            startArgument.addChild(argument("endPeriod", FloatArgumentType.floatArg(1))
-                    .executes(AddCommand::run).build());
-            nameArgument.addChild(startArgument);
-            addNode.addChild(nameArgument);
-
-            dispatcher.getRoot().addChild(mainCommand);
-            mainCommand.addChild(listNode);
-            mainCommand.addChild(rcNode);
-            mainCommand.addChild(tpNode);
-            mainCommand.addChild(deleteNode);
-            mainCommand.addChild(addNode);
+            registerCommand(literal("ctp").build(), dispatcher, subs);
+            registerCommand(literal("cattp").build(), dispatcher, subs);
+            registerCommand(literal("catteleport").build(), dispatcher, subs);
         });
+    }
+
+    private static void registerCommand(LiteralCommandNode<FabricClientCommandSource> mainCommand, CommandDispatcher<FabricClientCommandSource> dispatcher, List<LiteralCommandNode<FabricClientCommandSource>> subs) {
+        dispatcher.getRoot().addChild(mainCommand);
+
+        for(var node : subs) {
+            mainCommand.addChild(node);
+        }
+    }
+
+    private static List<LiteralCommandNode<FabricClientCommandSource>> generateSubcommands() {
+        // List SubCommand
+        var listNode = literal("list").executes(ListCommand::run).build();
+
+        // RC SubCommand
+        var rcNode = literal("rc").build();
+        rcNode.addChild(argument("seconds", IntegerArgumentType.integer(1))
+                .executes(RcCommand::run).build());
+
+        // TP SubCommand
+        var tpNode = literal("tp").build();
+        tpNode.addChild(argument("name", StringArgumentType.word())
+                .suggests(TpCommand::suggest).executes(TpCommand::run).build());
+
+        // Delete SubCommand
+        var deleteNode = literal("delete").build();
+        deleteNode.addChild(argument("name", StringArgumentType.word())
+                .suggests(DeleteCommand::suggest).executes(DeleteCommand::run).build());
+
+        // Add SubCommand
+        var addNode = literal("add").build();
+        var nameArgument = argument("name", StringArgumentType.word()).build();
+        var startArgument = argument("startPeriod", FloatArgumentType.floatArg(0)).build();
+        startArgument.addChild(argument("endPeriod", FloatArgumentType.floatArg(1))
+                .executes(AddCommand::run).build());
+        nameArgument.addChild(startArgument);
+        addNode.addChild(nameArgument);
+
+        return List.of(listNode, rcNode, tpNode, deleteNode, addNode);
     }
 }
